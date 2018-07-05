320b7b2dd7b1400c8181535af63ac6c9065cc48b
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-05 17:51:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,5 +11,5 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # project.css("p.bbcard_blurb").text
   # project.css("span.location-name").text
-  # project.css("ul.project-stats li.first.funded strong").text
+  # project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 end
